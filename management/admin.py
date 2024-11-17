b6944395c433511f2c200208c196eda810d215ed
6hislain <ghislainmike80@gmail.com>
2024-11-17 14:24:19 +0200

    first commit

---
+++ management/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .model import (
+    CitizenParent,
+    Citizen,
+    Colline,
+    Commune,
+    IdCardRegistration,
+    LostIdCardReport,
+    Province,
+    Publication,
+    RegisteredIdCard,
+    RegisteredIdCardApplication,
+    Service,
+)
+
+# Register your models here.
+admin.site.register(CitizenParent)
+admin.site.register(Citizen)
+admin.site.register(Colline)
+admin.site.register(Commune)
+admin.site.register(IdCardRegistration)
+admin.site.register(LostIdCardReport)
+admin.site.register(Province)
+admin.site.register(Publication)
+admin.site.register(RegisteredIdCard)
+admin.site.register(RegisteredIdCardApplication)
+admin.site.register(Service)
---
+++ management/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+
+# Create your models here.
+class CitizenParent(models.Model):
+    pass
+
+
+class Citizen(models.Model):
+    pass
+
+
+class Colline(models.Model):
+    pass
+
+
+class Commune(models.Model):
+    pass
+
+
+class IdCardRegistration(models.Model):
+    pass
+
+
+class LostIdCardReport(models.Model):
+    pass
+
+
+class Province(models.Model):
+    pass
+
+
+class Publication(models.Model):
+    pass
+
+
+class RegisteredIdCard(models.Model):
+    pass
+
+
+class RegisteredIdCardApplication(models.Model):
+    pass
+
+
+class Service(models.Model):
+    pass
