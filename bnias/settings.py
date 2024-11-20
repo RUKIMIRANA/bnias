76e103c946d26ec7d11a18ac318e450ac97bcadd
6hislain <ghislainmike80@gmail.com>
2024-11-20 10:57:46 +0200

    home page images

--- bnias/settings.py
+++ bnias/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = "bnias.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
-    # "default": dj_database_url.parse(os.getenv("DATABASE_URL"))
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # },
+    "default": dj_database_url.parse(os.getenv("DATABASE_URL"))
 }
 
 DB_PREFIX = "bnias_"
