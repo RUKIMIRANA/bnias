aa2c9eef1c8adc5fcebc40aa0230ee3f003749dc
6hislain <ghislainmike80@gmail.com>
2024-11-21 12:19:23 +0200

    db prefix

--- bnias/settings.py
+++ bnias/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "django_database_prefix",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
