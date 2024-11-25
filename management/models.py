13ebcbcac0c51a84f63d9eae3db475a014102462
6hislain <ghislainmike80@gmail.com>
2024-11-25 14:28:47 +0200

    provinces

--- management/models.py
+++ management/models.py
@@ -9,6 +9,9 @@ class CitizenParent(models.Model):
     gender = models.CharField(max_length=10)
     birth_date = models.DateField(auto_now=False, auto_now_add=False)
 
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 
 class Citizen(models.Model):
     first_name = models.CharField(max_length=50)
@@ -19,13 +22,31 @@ class Citizen(models.Model):
     father_name = models.CharField(max_length=50)
     mother_name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 
-class Colline(models.Model):
+
+class Province(models.Model):
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
 
 
 class Commune(models.Model):
     name = models.CharField(max_length=30)
+    province = models.ForeignKey(Province, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Colline(models.Model):
+    name = models.CharField(max_length=30)
+    commune = models.ForeignKey(Commune, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class IdCardRegistration(models.Model):
@@ -34,10 +55,6 @@ class IdCardRegistration(models.Model):
 
 class LostIdCardReport(models.Model):
     citizen_id = models.IntegerField()
-
-
-class Province(models.Model):
-    name = models.CharField(max_length=30)
 
 
 class Publication(models.Model):
@@ -61,7 +78,13 @@ class RegisteredIdCardApplication(models.Model):
     picture = models.ImageField(upload_to="applicants/")
     residence = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 
 class Service(models.Model):
     name = models.CharField(max_length=20)
     requirement = models.TextField()
+
+    def __str__(self):
+        return self.name
