286b91166a3ce3e6b06bdf71e8f1737e18b02d4b
6hislain <ghislainmike80@gmail.com>
2024-11-26 11:32:25 +0200

    approve applicant

--- management/models.py
+++ management/models.py
@@ -71,6 +71,7 @@ class RegisteredIdCardApplication(models.Model):
     phone_number = models.IntegerField()
     picture = models.ImageField(upload_to="applicants/")
     residence = models.CharField(max_length=50)
+    is_approved = models.BooleanField(default=False)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
